52bc3258b0aa806a60f6c3472c01d4662e194188
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-02-28 11:42:53 -0500

    Preparing the BrushSizeSelector and How To Use DisplayMetrics

--- app/src/main/java/ir/iammrbit/paint/DrawingView.kt
+++ app/src/main/java/ir/iammrbit/paint/DrawingView.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -36,7 +37,7 @@ class DrawingView(context: Context , attrs : AttributeSet)
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20F
+//        mBrushSize = 20F
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -90,6 +91,11 @@ class DrawingView(context: Context , attrs : AttributeSet)
 
     }
 
+    fun setSizeForBrush(newSize : Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP
+            , newSize,resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
 
     internal inner class CustomPath(var color: Int , var brushThickness : Float)
         : android.graphics.Path(){
--- app/src/main/java/ir/iammrbit/paint/MainActivity.kt
+++ app/src/main/java/ir/iammrbit/paint/MainActivity.kt
@@ -2,10 +2,18 @@ package ir.iammrbit.paint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.iammrbit.paint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var drawingView:DrawingView? = null
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        drawingView = binding.drawingView
+        drawingView?.setSizeForBrush(20F)
     }
 }
