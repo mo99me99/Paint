bc0ffa27b2e2ec279ac7278f53dccdac38e0ea41
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-03-08 08:52:03 -0500

    add select image from gallery

--- app/src/main/java/ir/iammrbit/paint/MainActivity.kt
+++ app/src/main/java/ir/iammrbit/paint/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import android.Manifest
+import android.content.Intent
+import android.provider.MediaStore
+import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import ir.iammrbit.paint.databinding.ActivityMainBinding
 
@@ -19,6 +22,14 @@ import ir.iammrbit.paint.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private var drawingView:DrawingView? = null
     private var mImageButtonCurrentPaint : ImageButton? = null
+    private val openGalleryLauncher : ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if (result.resultCode == RESULT_OK && result.data!=null){
+                val imageBG : ImageView = binding.ivBackground
+                imageBG.setImageURI(result.data?.data)
+            }
+        }
 
     private val requestPermission : ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
@@ -32,6 +43,11 @@ class MainActivity : AppCompatActivity() {
                             "Permission granted. Now you can read the storage files."
                             ,Toast.LENGTH_SHORT)
                             .show()
+                        val pickIntent = Intent(
+                            Intent.ACTION_PICK , MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        openGalleryLauncher.launch(pickIntent)
+
+
                     }else{
                         if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
                             Toast.makeText(
@@ -131,24 +147,11 @@ class MainActivity : AppCompatActivity() {
     private fun showRationalDialog(
         title : String,
         message : String
-    ){
-        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(title)
             .setMessage(message)
-            .setPositiveButton("Cancel"){dialog , _-> dialog.dismiss()}
+            .setPositiveButton("Cancel") { dialog, _ -> dialog.dismiss() }
         builder.create().show()
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
